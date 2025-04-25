e8835481f5371e2a2a117578396fb4ae875b3ca9
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2025-04-25 16:22:24 +0200

    Add basic tests for parsing

--- hayro-syntax/src/function/type4.rs
+++ hayro-syntax/src/function/type4.rs
@@ -2,16 +2,49 @@ use crate::file::xref::XRef;
 use crate::function::CommonProperties;
 use crate::object::number::{InternalNumber, Number};
 use crate::reader::Reader;
+use crate::{OptionLog, content};
 use log::{debug, error};
+use smallvec::{SmallVec, smallvec};
+
+type ParseStack = SmallVec<[Vec<PostScriptOp>; 2]>;
 
 struct Type4 {
     common: CommonProperties,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq)]
+fn parse_procedure(data: &[u8]) -> Option<Vec<PostScriptOp>> {
+    let mut r = Reader::new(data);
+    parse_procedure_inner(&mut r).warn_none("failed to read postscript program")
+}
+
+fn parse_procedure_inner(r: &mut Reader) -> Option<Vec<PostScriptOp>> {
+    let mut stack: ParseStack = smallvec![];
+
+    let mut ops = vec![];
+    r.skip_white_spaces_and_comments();
+    r.forward_tag(b"{")?;
+
+    loop {
+        r.skip_white_spaces_and_comments();
+
+        if r.peek_byte()? == b'}' {
+            r.forward_tag(b"}")?;
+
+            break;
+        } else if r.peek_byte()? == b'{' {
+            stack.push(parse_procedure_inner(r)?);
+        } else {
+            let op = PostScriptOp::from_reader(r, &mut stack)?;
+            ops.push(op);
+        }
+    }
+
+    Some(ops)
+}
+
+#[derive(Debug, Clone, PartialEq)]
 pub enum PostScriptOp {
-    Real(f32),
-    Integer(i32),
+    Number(Number),
     Abs,
     Add,
     Atan,
@@ -46,8 +79,8 @@ pub enum PostScriptOp {
     Or,
     True,
     Xor,
-    If,
-    IfElse,
+    If(Vec<PostScriptOp>),
+    IfElse(Vec<PostScriptOp>, Vec<PostScriptOp>),
     Copy,
     Dup,
     Exch,
@@ -57,15 +90,13 @@ pub enum PostScriptOp {
 }
 
 impl PostScriptOp {
-    fn from_token(data: &[u8]) -> Option<Self> {
-        let mut r = Reader::new(data);
+    fn from_reader(r: &mut Reader, stack: &mut ParseStack) -> Option<Self> {
         let op = if let Some(n) = r.read::<true, Number>(&XRef::dummy()) {
-            match n.0 {
-                InternalNumber::Real(r) => PostScriptOp::Real(r),
-                InternalNumber::Integer(i) => PostScriptOp::Integer(i),
-            }
+            // TODO: Support radix numbers
+            Self::Number(n)
         } else {
-            match data {
+            let op = r.read::<true, content::Operator>(&XRef::dummy())?.get();
+            match op.as_ref() {
                 b"abs" => Self::Abs,
                 b"add" => Self::Add,
                 b"atan" => Self::Atan,
@@ -100,8 +131,12 @@ impl PostScriptOp {
                 b"or" => Self::Or,
                 b"true" => Self::True,
                 b"xor" => Self::Xor,
-                b"if" => Self::If,
-                b"ifelse" => Self::IfElse,
+                b"if" => Self::If(stack.pop()?),
+                b"ifelse" => {
+                    let s = stack.pop()?;
+                    let f = stack.pop()?;
+                    Self::IfElse(f, s)
+                }
                 b"copy" => Self::Copy,
                 b"dup" => Self::Dup,
                 b"exch" => Self::Exch,
@@ -109,7 +144,7 @@ impl PostScriptOp {
                 b"pop" => Self::Pop,
                 b"roll" => Self::Roll,
                 _ => {
-                    error!("encountered unknown postscript operator {:?}", data);
+                    error!("encountered unknown postscript operator {:?}", op);
 
                     return None;
                 }
@@ -117,5 +152,45 @@ impl PostScriptOp {
         };
 
         Some(op)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::function::type4::{PostScriptOp, parse_procedure};
+    use crate::object::number::Number;
+
+    #[test]
+    fn lex_1() {
+        let program = b"{ copy dup 0.4545 exch roll }";
+        let parsed = parse_procedure(program).unwrap();
+
+        assert_eq!(
+            parsed,
+            vec![
+                PostScriptOp::Copy,
+                PostScriptOp::Dup,
+                PostScriptOp::Number(Number::from_f32(0.4545)),
+                PostScriptOp::Exch,
+                PostScriptOp::Roll,
+            ]
+        )
+    }
+
+    #[test]
+    fn lex_3() {
+        let program = b" {  {dup exch} if {0} {1} ifelse }";
+        let parsed = parse_procedure(program).unwrap();
+
+        assert_eq!(
+            parsed,
+            vec![
+                PostScriptOp::If(vec![PostScriptOp::Dup, PostScriptOp::Exch]),
+                PostScriptOp::IfElse(
+                    vec![PostScriptOp::Number(Number::from_i32(0))],
+                    vec![PostScriptOp::Number(Number::from_i32(1))]
+                )
+            ]
+        )
     }
 }
