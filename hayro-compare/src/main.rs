03e6b97d3a671334744fce8d9a5280d15b942e73
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2025-04-29 19:16:47 +0200

    Limit the number of pages to 3

--- hayro-compare/src/main.rs
+++ hayro-compare/src/main.rs
@@ -4,6 +4,8 @@ use sitro::RenderOptions;
 use std::path::{Path, PathBuf};
 use walkdir::WalkDir;
 
+const MAX_PAGES: usize = 3;
+
 fn main() {
     if let Ok(()) = log::set_logger(&LOGGER) {
         log::set_max_level(log::LevelFilter::Warn);
@@ -41,7 +43,7 @@ fn render_pdfium(entries: &[PathBuf]) {
         let file = std::fs::read(path).unwrap();
         let pages = sitro::render_pdfium(&file, &RenderOptions::default()).unwrap();
 
-        for (idx, page) in pages.iter().enumerate() {
+        for (idx, page) in pages.iter().enumerate().take(MAX_PAGES) {
             let suffix = if pages.len() == 1 {
                 "".to_string()
             } else {
@@ -67,7 +69,7 @@ fn render_hayro(entries: &[PathBuf]) {
         let pdf = Pdf::new(&data).unwrap();
         let pages = hayro_render::render_png(&pdf, 1.0);
 
-        for (idx, page) in pages.iter().enumerate() {
+        for (idx, page) in pages.iter().enumerate().take(MAX_PAGES) {
             let suffix = if pages.len() == 1 {
                 "".to_string()
             } else {
