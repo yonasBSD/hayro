d29ad0f924c09320800806041474b19fdee91dd0
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2025-05-13 19:06:00 +0200

    Remove two unnecessary variables

--- hayro-syntax/src/bit.rs
+++ hayro-syntax/src/bit.rs
@@ -21,22 +21,14 @@ pub struct BitReader<'a> {
     data: &'a [u8],
     cur_pos: usize,
     bit_size: BitSize,
-    max: usize,
-    count: usize,
 }
 
 impl<'a> BitReader<'a> {
     pub fn new(data: &'a [u8], bit_size: BitSize) -> Self {
-        Self::new_with(data, bit_size, 0, None)
-    }
-
-    fn new_with(data: &'a [u8], bit_size: BitSize, cur_pos: usize, max: Option<usize>) -> Self {
         Self {
             data,
             bit_size,
-            cur_pos,
-            count: 0,
-            max: max.unwrap_or(usize::MAX),
+            cur_pos: 0,
         }
     }
 
@@ -64,10 +56,6 @@ impl<'a> Iterator for BitReader<'a> {
         let byte_pos = self.byte_pos();
 
         if byte_pos >= self.data.len() {
-            return None;
-        }
-
-        if self.count >= self.max {
             return None;
         }
 
@@ -99,15 +87,13 @@ impl<'a> Iterator for BitReader<'a> {
                 let bit_pos = self.bit_pos();
                 let advance = self.bit_size.bits();
                 let item = (self.data[byte_pos] as u16 >> (8 - bit_pos - advance))
-                    & self.bit_size.mask() as u16;
+                    & self.bit_size.mask();
 
                 self.cur_pos += advance;
 
                 Some(item)
             }
         }?;
-
-        self.count += 1;
 
         Some(item)
     }
@@ -272,18 +258,6 @@ mod tests {
         assert_eq!(reader.next().unwrap(), 0b0);
         assert_eq!(reader.next().unwrap(), 0b1);
         assert_eq!(reader.next().unwrap(), 0b0);
-        assert_eq!(reader.next().unwrap(), 0b0);
-        assert_eq!(reader.next().unwrap(), 0b0);
-        assert_eq!(reader.next().unwrap(), 0b0);
-    }
-
-    #[test]
-    fn bit_reader_start_pos_and_max() {
-        let data = [0b10011000, 0b00010000];
-        let mut reader = BitReader::new_with(&data, BitSize::from_u8(1).unwrap(), 6, Some(5));
-        assert_eq!(reader.next().unwrap(), 0b0);
-        assert_eq!(reader.next().unwrap(), 0b0);
-
         assert_eq!(reader.next().unwrap(), 0b0);
         assert_eq!(reader.next().unwrap(), 0b0);
         assert_eq!(reader.next().unwrap(), 0b0);
