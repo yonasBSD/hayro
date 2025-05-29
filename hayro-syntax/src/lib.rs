97b7bc6ce96547624f49105d6e4d955b7ce4aecc
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2025-05-29 17:09:34 +0200

    Remove old NOTICE file

--- hayro-syntax/src/lib.rs
+++ hayro-syntax/src/lib.rs
@@ -12,13 +12,14 @@ the PDF parsing logic. The supported features include:
 
 This crate does not provide more high-level functionality, such as parsing fonts or color spaces.
 Such functionality is out-of-scope for `hayro-syntax`, since this crate is supposed to be
-as light-weight and application-agnostic as possible.
+as light-weight and application-agnostic as possible. Functionality-wise, this crate is therefore
+pretty much feature-complete, though more low-level APIs might be added in the future.
 
 # Features
 This crate has one feature, `jpeg2000`. PDF allows for the insertion of JPEG2000 images. However,
 unfortunately, JPEG2000 is a very complicated format. There exists a Rust crate that allows decoding
 such images (which is also used by `hayro-syntax`), but it is a very heavy dependency, has a lot of
-unsafe code (due to being ported with c2rust), and also has a dependency on libc, meaning that you
+unsafe code (due to having been ported with `c2rust`), and also has a dependency on libc, meaning that you
 might be restricted in the targets you can build to. Because of this, I recommend not enabling this
 feature, unless you absolutely need to be able to support such images. The main reason why this
 */
