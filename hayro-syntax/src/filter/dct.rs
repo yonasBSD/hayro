55e566cbe68d71c28b73863c987e9e9e93b445b9
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2025-06-07 11:09:18 +0200

    Fix non-rendering of JPEG image

--- hayro-syntax/src/filter/dct.rs
+++ hayro-syntax/src/filter/dct.rs
@@ -10,7 +10,7 @@ pub fn decode(data: &[u8], _: Dict) -> Option<Vec<u8>> {
     let mut decoder = zune_jpeg::JpegDecoder::new(data);
     decoder.decode_headers().ok()?;
 
-    let out_colorspace = match decoder.get_input_colorspace().unwrap() {
+    let mut out_colorspace = match decoder.get_input_colorspace().unwrap() {
         ColorSpace::RGB | ColorSpace::RGBA | ColorSpace::YCbCr => ColorSpace::RGB,
         ColorSpace::Luma | ColorSpace::LumaA => ColorSpace::Luma,
         ColorSpace::CMYK => ColorSpace::CMYK,
@@ -19,7 +19,19 @@ pub fn decode(data: &[u8], _: Dict) -> Option<Vec<u8>> {
     };
 
     decoder.set_options(DecoderOptions::default().jpeg_set_out_colorspace(out_colorspace));
-    let mut decoded = decoder.decode().unwrap();
+    let mut decoded = decoder.decode().ok().or_else(|| {
+        // It's possible that the APP14 marker is set, so that zune_jpeg will set the input colorspace
+        // to a different one. So try decoding again with the different color space. This is probably
+        // not the proper way to solve this, but it solves a test case.
+        if matches!(out_colorspace, ColorSpace::YCCK | ColorSpace::CMYK) {
+            out_colorspace = ColorSpace::RGB;
+        }   else {
+            out_colorspace = ColorSpace::CMYK;
+        }
+
+        decoder.set_options(DecoderOptions::default().jpeg_set_out_colorspace(out_colorspace));
+        decoder.decode().ok()
+    })?;
 
     if out_colorspace == ColorSpace::YCCK {
         // See <https://github.com/mozilla/pdf.js/blob/69595a29192b7704733404a42a2ebb537601117b/src/core/jpg.js#L1331>
