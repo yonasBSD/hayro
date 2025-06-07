37ad950b6a20579b9e900c48ecc5474ea38c46d8
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2025-06-07 17:12:07 +0200

    Fix bug in LZW decoder

--- hayro-syntax/src/filter/lzw_flate.rs
+++ hayro-syntax/src/filter/lzw_flate.rs
@@ -63,7 +63,7 @@ pub mod lzw {
     fn decode_impl(data: &[u8], early_change: bool) -> Option<Vec<u8>> {
         let mut table = Table::new(early_change);
 
-        let bit_size = BitSize::from_u8(table.code_length())?;
+        let mut bit_size = BitSize::from_u8(table.code_length())?;
         let mut reader = BitReader::new(data);
         let mut decoded = vec![];
         let mut prev = None;
@@ -75,6 +75,7 @@ pub mod lzw {
                 CLEAR_TABLE => {
                     table.clear();
                     prev = None;
+                    bit_size = BitSize::from_u8(table.code_length())?;
                 }
                 EOD => return Some(decoded),
                 new => {
@@ -91,6 +92,7 @@ pub mod lzw {
                         decoded.extend_from_slice(table.register(prev, new_byte)?);
                     }
 
+                    bit_size = BitSize::from_u8(table.code_length())?;
                     prev = Some(new);
                 }
             }
