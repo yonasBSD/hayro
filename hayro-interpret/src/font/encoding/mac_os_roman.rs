9eed1ebc2301e359e53e4b9746c055e68373b1cc
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2025-06-08 22:00:15 +0200

    Use decimal numbers for the phf map

--- hayro-interpret/src/font/encoding/mac_os_roman.rs
+++ hayro-interpret/src/font/encoding/mac_os_roman.rs
@@ -2,22 +2,22 @@
 use phf::phf_map;
 
 pub(crate) static MAC_OS_ROMAN: phf::Map<u8, &'static str> = phf_map! {
-    0o0255u8 => "notequal",
-    0o0260u8 => "infinity",
-    0o0262u8 => "lessequal",
-    0o0263u8 => "greaterequal",
-    0o0266u8 => "partialdiff",
-    0o0267u8 => "summation",
-    0o0270u8 => "product",
-    0o0271u8 => "pi",
-    0o0272u8 => "integral",
-    0o0275u8 => "Omega",
-    0o0303u8 => "radical",
-    0o0305u8 => "approxequal",
-    0o0306u8 => "Delta",
-    0o0327u8 => "lozenge",
-    0o0333u8 => "Euro",
-    0o0360u8 => "apple"
+    173u8 => "notequal",
+    176u8 => "infinity",
+    178u8 => "lessequal",
+    179u8 => "greaterequal",
+    182u8 => "partialdiff",
+    183u8 => "summation",
+    184u8 => "product",
+    185u8 => "pi",
+    186u8 => "integral",
+    189u8 => "Omega",
+    195u8 => "radical",
+    197u8 => "approxequal",
+    198u8 => "Delta",
+    215u8 => "lozenge",
+    219u8 => "Euro",
+    240u8 => "apple"
 };
 
 pub(crate) static MAC_OS_ROMAN_INVERSE: phf::Map<&'static str, u8> = phf_map! {
