c5f49472d60b857bc5f05672116498dc4a94cc94
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2025-08-19 22:21:04 +0200

    Improve the documentation of the `hayro` crate (#200)

--- hayro/src/lib.rs
+++ hayro/src/lib.rs
@@ -1,16 +1,34 @@
 /*!
 A crate for rendering PDF files.
 
-This crate allows you to render PDF files into a pixmap. For more information on how to
-use this crate, see the [example](https://github.com/LaurenzV/hayro/tree/master/hayro/examples) in
+This crate allows you to render pages of a PDF file into bitmaps. It is supposed to be relatively
+lightweight, since we do not have any dependencies on the GPU. All the rendering happens on the CPU.
+
+The ultimate goal of this crate is to be a *feature-complete* and *performant* PDF rasterizer.
+With that said, we are currently still very far away from reaching that goal: So far, no effort
+has been put into performance optimizations, as we are still working on implementing missing features.
+However, this crate is currently the most comprehensive and feature-complete
+implementation of a PDF rasterizer in pure Rust. This claim is supported by the fact that we currently
+include over 1000 PDF files in our regression test suite. The majority of those have been scraped
+from the `pdf.js` and `PDFBOX` test suites and therefore represent a very large and diverse sample
+of PDF files.
+
+As mentioned, there are still some serious limitations, including lack of support for
+encrypted/password-protected PDF files, blending and isolation, knockout groups as well as a range
+of smaller features such as color key masking. But you should be able to render the vast majority
+of PDF files without too many issues.
+
+## Safety
+This crate forbids unsafe code via a crate-level attribute.
+
+## Examples
+For usage examples, see the [example](https://github.com/LaurenzV/hayro/tree/master/hayro/examples) in
 the GitHub repository.
 
-This crate is still very much in development, because of this, documentation is currently still
-very lacking.
-
 ## Cargo features
-This crate has one feature, `jpeg2000`. See the description of
-[`hayro-syntax`](https://docs.rs/hayro-syntax/latest/hayro_syntax/#cargo-features) for more information.
+This crate has two optional features:
+- `jpeg2000`: See the description of [`hayro-syntax`](https://docs.rs/hayro-syntax/latest/hayro_syntax/#cargo-features) for more information.
+- `embed-fonts`: See the description of [`hayro-interpret`](https://docs.rs/hayro-interpret/latest/hayro_interpret/#cargo-features) for more information.
 */
 
 #![forbid(unsafe_code)]
