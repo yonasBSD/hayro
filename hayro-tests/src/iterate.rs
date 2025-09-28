475f3c787bc9e919018fc738720c55d1511882a5
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2025-09-28 17:22:22 +0200

    Add a ignore list in the `iterate` binary (#312)

--- hayro-tests/src/iterate.rs
+++ hayro-tests/src/iterate.rs
@@ -1,10 +1,21 @@
 use hayro::Pdf;
+use memchr::memmem::Finder;
 use rayon::prelude::*;
 use std::env;
 use std::fs;
 use std::path::PathBuf;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use walkdir::WalkDir;
+
+static IGNORE_LIST: &[&str] = &[
+    // Password-protected
+    "0000300", // Broken PDF, maybe fixable
+    "0000399", // HTML
+    "0000819", "0000920", "0001589", "0002064", "0002187", "0002244", "0002372", "0002554",
+    "0002638", "0002966", "0003269", "0003892", "0003927", "0003983", "0004537", "0004889",
+    "0004997", "0006169", "0006207", "0006339", "0006844", "0008443", "0008674", "0008978",
+    "0009309", "0009464", "0009706", "0010117", "0010216", "0010902",
+];
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -32,11 +43,34 @@ fn main() {
 
     println!("Found {} PDF files", pdf_paths.len());
 
+    let entries = Mutex::new(vec![]);
+
     pdf_paths.par_iter().for_each(|path| {
         let data = Arc::new(fs::read(path).unwrap());
-        match Pdf::new(data) {
+        let name = path.file_stem().unwrap().to_str().unwrap().to_string();
+
+        if IGNORE_LIST.contains(&name.as_str()) {
+            return;
+        }
+
+        match Pdf::new(data.clone()) {
             Ok(_) => {}
-            Err(e) => println!("  ✗ Failed to load PDF {path:?}: {e:?}"),
+            Err(e) => {
+                let finder = Finder::new("html");
+                let reason = if finder.find(data.as_slice()).is_some() {
+                    "html".to_string()
+                } else {
+                    format!("{:?}", e)
+                };
+                entries.lock().unwrap().push((name, reason));
+            }
         }
     });
+
+    let mut inner = Mutex::into_inner(entries).unwrap();
+    inner.sort_by(|(a, _), (b, _)| a.cmp(b));
+
+    for entry in inner {
+        println!("{} - {}", entry.0, entry.1);
+    }
 }
