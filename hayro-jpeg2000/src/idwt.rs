36c8585df92181a5fafc5603812db6610e586b2e
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2025-11-05 14:53:02 +0100

    hayro-jpeg2000: Improve documentation of IDWT

--- hayro-jpeg2000/src/idwt.rs
+++ hayro-jpeg2000/src/idwt.rs
@@ -5,10 +5,14 @@ use crate::packet::{Decomposition, SubBand, SubbandType};
 use crate::tile::IntRect;
 use std::iter;
 
+/// The amount of padding to apply to a single scanline to make filtering at
+/// the boundary possible.
 const PADDING_SHIFT: usize = 4;
 
 pub(crate) fn apply(
+    // The lower LL subband for resolution level 0.
     ll_subband: &SubBand,
+    // All decomposition level that make up the tile.
     decompositions: &[Decomposition],
     tile_rect: IntRect,
     transform: WaveletTransform,
@@ -18,14 +22,7 @@ pub(crate) fn apply(
     for decomposition in decompositions {
         let ll_rect = decomposition.sub_bands[0].ll_rect;
 
-        ll_subband = _2d_sr(
-            &ll_subband,
-            &decomposition.sub_bands[0],
-            &decomposition.sub_bands[1],
-            &decomposition.sub_bands[2],
-            ll_rect,
-            transform,
-        );
+        ll_subband = filter_2d(&ll_subband, &decomposition, ll_rect, transform);
     }
 
     let mut trimmed_coefficients = Vec::with_capacity(ll_subband.coefficients.len());
@@ -47,18 +44,17 @@ pub(crate) fn apply(
     trimmed_coefficients
 }
 
-fn _2d_sr(
+/// The 2D_INTERLEAVE procedure described in F.3.3.
+fn filter_2d(
     ll: &SubBand,
-    hl: &SubBand,
-    lh: &SubBand,
-    hh: &SubBand,
+    decomposition: &Decomposition,
     rect: IntRect,
     transform: WaveletTransform,
 ) -> SubBand<'static> {
-    let mut coefficients = _2d_interleave(ll, hl, lh, hh, rect);
+    let mut coefficients = interleave_samples(ll, decomposition, rect);
 
-    hor_sr(&mut coefficients, rect, &transform);
-    ver_sr(&mut coefficients, rect, &transform);
+    filter_horizontal(&mut coefficients, rect, &transform);
+    filter_vertical(&mut coefficients, rect, &transform);
 
     SubBand {
         sub_band_type: SubbandType::LowLow,
@@ -69,13 +65,8 @@ fn _2d_sr(
     }
 }
 
-fn _2d_interleave(
-    ll: &SubBand,
-    hl: &SubBand,
-    lh: &SubBand,
-    hh: &SubBand,
-    rect: IntRect,
-) -> Vec<f32> {
+/// The 2D_INTERLEAVE procedure described in F.3.3.
+fn interleave_samples(ll: &SubBand, decomposition: &Decomposition, rect: IntRect) -> Vec<f32> {
     let mut coefficients = vec![0.0; (rect.width() * rect.height()) as usize];
     let IntRect {
         x0: u0,
@@ -84,7 +75,12 @@ fn _2d_interleave(
         y1: v1,
     } = rect;
 
-    for subband in [ll, hl, lh, hh] {
+    for subband in [
+        ll,
+        &decomposition.sub_bands[0],
+        &decomposition.sub_bands[1],
+        &decomposition.sub_bands[2],
+    ] {
         let (u_min, u_max) = match subband.sub_band_type {
             SubbandType::LowLow | SubbandType::LowHigh => (u0.div_ceil(2), u1.div_ceil(2)),
             SubbandType::HighLow | SubbandType::HighHigh => (u0 / 2, u1 / 2),
@@ -114,9 +110,21 @@ fn _2d_interleave(
 }
 
 /// The HOR_SR procedure from F.3.4.
-fn hor_sr(a: &mut [f32], rect: IntRect, transform: &WaveletTransform) {
+fn filter_horizontal(scan_line: &mut [f32], rect: IntRect, transform: &WaveletTransform) {
     // Add a padding of 8 to account for the _1d_extr procedure.
     let mut buf = vec![0.0; rect.width() as usize + 10];
+
+    // There's some subtlety going on here. The extension procedure defined in
+    // the spec is based on the start and end values i0 and i1 which are
+    // dependent on the rectangle of the subband we are currently processing.
+    // The problem is that if we use the values as is, if we for example had the
+    // i0/i1 values larger than 1000, we would have to allocate a buffer of
+    // length at least 1000, even though the width/height of the rectangle is
+    // much less. Looking at the equations more closely, it becomes apparent
+    // that the real value of i0/i1 is not relevant, and the behavior of
+    // subsequent operations only really depends on whether val % 2 == 0 or
+    // not. Therefore, we shift the values of i0 and i1 such that the property
+    // still remains the same, but the values themselves are much smaller.
 
     let shift = PADDING_SHIFT + if !rect.x0.is_multiple_of(2) { 1 } else { 0 };
 
@@ -128,24 +136,26 @@ fn hor_sr(a: &mut [f32], rect: IntRect, transform: &WaveletTransform) {
         let start_idx = rect.width() as usize * v as usize;
 
         // Extract row into buffer.
-        buf.extend_from_slice(&a[start_idx..][..rect.width() as usize]);
+        buf.extend_from_slice(&scan_line[start_idx..][..rect.width() as usize]);
 
         // Add right padding for 1D_EXTR procedure.
         buf.extend(iter::repeat_n(0.0, shift));
 
-        _1d_sr(&mut buf, shift, shift + rect.width() as usize, transform);
+        filter_single_row(&mut buf, shift, shift + rect.width() as usize, transform);
 
         // Put values back into original array.
-        a[start_idx..][..rect.width() as usize]
+        scan_line[start_idx..][..rect.width() as usize]
             .copy_from_slice(&buf[shift..][..rect.width() as usize]);
     }
 }
 
 /// The VER_SR procedure from F.3.5.
-fn ver_sr(a: &mut [f32], rect: IntRect, transform: &WaveletTransform) {
-    // Add a padding of 8 to account for the _1d_extr procedure.
+fn filter_vertical(a: &mut [f32], rect: IntRect, transform: &WaveletTransform) {
+    // Add a padding of 8 to account for the 1D_EXTR procedure.
+    // TODO: Reuse buffer.
     let mut buf = vec![0.0; rect.height() as usize + 10];
 
+    // See the comment in `filter_horizontal`.
     let shift = PADDING_SHIFT + if !rect.y0.is_multiple_of(2) { 1 } else { 0 };
 
     for u in 0..rect.width() {
@@ -161,7 +171,7 @@ fn ver_sr(a: &mut [f32], rect: IntRect, transform: &WaveletTransform) {
         // Add right padding for 1D_EXTR procedure.
         buf.extend(iter::repeat_n(0.0, shift));
 
-        _1d_sr(&mut buf, shift, shift + rect.height() as usize, transform);
+        filter_single_row(&mut buf, shift, shift + rect.height() as usize, transform);
 
         // Put values back into original array.
         for (idx, y) in (0..rect.height()).enumerate() {
@@ -170,41 +180,42 @@ fn ver_sr(a: &mut [f32], rect: IntRect, transform: &WaveletTransform) {
     }
 }
 
-/// The 1D_SR procedure from F.3.6
-fn _1d_sr(y: &mut [f32], i0: usize, i1: usize, transform: &WaveletTransform) {
-    if i0 == i1 - 1 {
-        if !i0.is_multiple_of(2) {
-            y[i0] /= 2.0;
+/// The 1D_SR procedure from F.3.6.
+fn filter_single_row(scanline: &mut [f32], start: usize, end: usize, transform: &WaveletTransform) {
+    if start == end - 1 {
+        if !start.is_multiple_of(2) {
+            scanline[start] /= 2.0;
         }
 
         return;
     }
 
-    _1d_extr(y, i0, i1, transform);
+    extend_signal(scanline, start, end, transform);
 
     match transform {
-        WaveletTransform::Reversible53 => _1d_filter_53r(y, i0, i1),
-        WaveletTransform::Irreversible97 => _1d_filter_97(y, i0, i1),
+        WaveletTransform::Reversible53 => reversible_filter_53r(scanline, start, end),
+        WaveletTransform::Irreversible97 => irreversible_filter_97i(scanline, start, end),
     }
 }
 
 /// The 1D FILTER 5-3R procedure from F.3.8.1.
-fn _1d_filter_53r(y: &mut [f32], i0: usize, i1: usize) {
-    // (F-5)
-    for n in i0 / 2..(i1 / 2) + 1 {
+fn reversible_filter_53r(scanline: &mut [f32], start: usize, end: usize) {
+    // Equation (F-5).
+    for n in start / 2..(end / 2) + 1 {
         let base_idx = 2 * n;
-        y[base_idx] -= ((y[base_idx - 1] + y[base_idx + 1] + 2.0) / 4.0).floor();
+        scanline[base_idx] -=
+            ((scanline[base_idx - 1] + scanline[base_idx + 1] + 2.0) / 4.0).floor();
     }
 
-    // (F-6)
-    for n in i0 / 2..(i1 / 2) {
+    // Equation (F-6).
+    for n in start / 2..(end / 2) {
         let base_idx = 2 * n + 1;
-        y[base_idx] += ((y[base_idx - 1] + y[base_idx + 1]) / 2.0).floor();
+        scanline[base_idx] += ((scanline[base_idx - 1] + scanline[base_idx + 1]) / 2.0).floor();
     }
 }
 
-/// The 1D Filter 9-7I procedure from F.3.8.2
-fn _1d_filter_97(y: &mut [f32], i0: usize, i1: usize) {
+/// The 1D Filter 9-7I procedure from F.3.8.2.
+fn irreversible_filter_97i(scanline: &mut [f32], start: usize, end: usize) {
     // Table F.4.
     const ALPHA: f32 = -1.586_134_3;
     const BETA: f32 = -0.052_980_117;
@@ -212,49 +223,49 @@ fn _1d_filter_97(y: &mut [f32], i0: usize, i1: usize) {
     const DELTA: f32 = 0.443_506_87;
     const KAPPA: f32 = 1.230_174_1;
 
-    // Step 1
-    for i in (i0 / 2 - 1)..(i1 / 2 + 2) {
-        y[2 * i] *= KAPPA;
+    // Step 1.
+    for i in (start / 2 - 1)..(end / 2 + 2) {
+        scanline[2 * i] *= KAPPA;
     }
 
-    // Step 2
-    for i in (i0 / 2 - 2)..(i1 / 2 + 2) {
-        y[2 * i + 1] *= (1.0 / KAPPA);
+    // Step 2.
+    for i in (start / 2 - 2)..(end / 2 + 2) {
+        scanline[2 * i + 1] *= (1.0 / KAPPA);
     }
 
-    // Step 3
-    for i in (i0 / 2 - 1)..(i1 / 2 + 2) {
-        y[2 * i] -= DELTA * (y[2 * i - 1] + y[2 * i + 1]);
+    // Step 3.
+    for i in (start / 2 - 1)..(end / 2 + 2) {
+        scanline[2 * i] -= DELTA * (scanline[2 * i - 1] + scanline[2 * i + 1]);
     }
 
-    // Step 4
-    for i in (i0 / 2 - 1)..(i1 / 2 + 1) {
-        y[2 * i + 1] -= GAMMA * (y[2 * i] + y[2 * i + 2]);
+    // Step 4.
+    for i in (start / 2 - 1)..(end / 2 + 1) {
+        scanline[2 * i + 1] -= GAMMA * (scanline[2 * i] + scanline[2 * i + 2]);
     }
 
-    // Step 5
-    for i in (i0 / 2)..(i1 / 2 + 1) {
-        y[2 * i] -= BETA * (y[2 * i - 1] + y[2 * i + 1]);
+    // Step 5.
+    for i in (start / 2)..(end / 2 + 1) {
+        scanline[2 * i] -= BETA * (scanline[2 * i - 1] + scanline[2 * i + 1]);
     }
 
-    // Step 6
-    for i in (i0 / 2)..(i1 / 2) {
-        y[2 * i + 1] -= ALPHA * (y[2 * i] + y[2 * i + 2]);
+    // Step 6.
+    for i in (start / 2)..(end / 2) {
+        scanline[2 * i + 1] -= ALPHA * (scanline[2 * i] + scanline[2 * i + 2]);
     }
 }
 
-/// The 1D_EXTR procedure.
-fn _1d_extr(y: &mut [f32], i0: usize, i1: usize, transform: &WaveletTransform) {
+/// The 1D_EXTR procedure, defined in F.3.7.
+fn extend_signal(scanline: &mut [f32], start: usize, end: usize, transform: &WaveletTransform) {
     let i_left = match transform {
         WaveletTransform::Reversible53 => {
-            if i0.is_multiple_of(2) {
+            if start.is_multiple_of(2) {
                 1
             } else {
                 2
             }
         }
         WaveletTransform::Irreversible97 => {
-            if i0.is_multiple_of(2) {
+            if start.is_multiple_of(2) {
                 3
             } else {
                 4
@@ -264,14 +275,14 @@ fn _1d_extr(y: &mut [f32], i0: usize, i1: usize, transform: &WaveletTransform) {
 
     let i_right = match transform {
         WaveletTransform::Reversible53 => {
-            if i1.is_multiple_of(2) {
+            if end.is_multiple_of(2) {
                 2
             } else {
                 1
             }
         }
         WaveletTransform::Irreversible97 => {
-            if i1.is_multiple_of(2) {
+            if end.is_multiple_of(2) {
                 4
             } else {
                 3
@@ -279,20 +290,20 @@ fn _1d_extr(y: &mut [f32], i0: usize, i1: usize, transform: &WaveletTransform) {
         }
     };
 
-    for i in (i0 - i_left)..i0 {
-        y[i] = y[pse(i, i0, i1)];
+    for i in (start - i_left)..start {
+        scanline[i] = scanline[periodic_symmetric_extension(i, start, end)];
     }
 
-    for i in i1..(i1 + i_right) {
-        y[i] = y[pse(i, i0, i1)];
+    for i in end..(end + i_right) {
+        scanline[i] = scanline[periodic_symmetric_extension(i, start, end)];
     }
 }
 
-/// Equation (F-4).
-fn pse(i: usize, i0: usize, i1: usize) -> usize {
-    let span = 2 * (i1 as i32 - i0 as i32 - 1);
-    let m = (i as i32 - i0 as i32).rem_euclid(span);
-    (i0 as i32 + m.min(span - m)) as usize
+/// Perform the periodic symmetric extension, specified in Equation (F-4).
+fn periodic_symmetric_extension(idx: usize, start: usize, end: usize) -> usize {
+    let span = 2 * (end as i32 - start as i32 - 1);
+    let offset = (idx as i32 - start as i32).rem_euclid(span);
+    (start as i32 + offset.min(span - offset)) as usize
 }
 
 #[cfg(test)]
@@ -301,22 +312,22 @@ mod tests {
 
     #[test]
     fn pse() {
-        assert_eq!(super::pse(0, 3, 6), 4);
-        assert_eq!(super::pse(1, 3, 6), 5);
-        assert_eq!(super::pse(2, 3, 6), 4);
-        assert_eq!(super::pse(3, 3, 6), 3);
-        assert_eq!(super::pse(4, 3, 6), 4);
-        assert_eq!(super::pse(5, 3, 6), 5);
-        assert_eq!(super::pse(6, 3, 6), 4);
-        assert_eq!(super::pse(7, 3, 6), 3);
-        assert_eq!(super::pse(8, 3, 6), 4);
-        assert_eq!(super::pse(9, 3, 6), 5);
+        assert_eq!(super::periodic_symmetric_extension(0, 3, 6), 4);
+        assert_eq!(super::periodic_symmetric_extension(1, 3, 6), 5);
+        assert_eq!(super::periodic_symmetric_extension(2, 3, 6), 4);
+        assert_eq!(super::periodic_symmetric_extension(3, 3, 6), 3);
+        assert_eq!(super::periodic_symmetric_extension(4, 3, 6), 4);
+        assert_eq!(super::periodic_symmetric_extension(5, 3, 6), 5);
+        assert_eq!(super::periodic_symmetric_extension(6, 3, 6), 4);
+        assert_eq!(super::periodic_symmetric_extension(7, 3, 6), 3);
+        assert_eq!(super::periodic_symmetric_extension(8, 3, 6), 4);
+        assert_eq!(super::periodic_symmetric_extension(9, 3, 6), 5);
     }
 
     #[test]
     fn extend_1d() {
         let mut data = [0.0, 0.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 0.0, 0.0];
-        super::_1d_extr(&mut data, 3, 9, &WaveletTransform::Reversible53);
+        super::extend_signal(&mut data, 3, 9, &WaveletTransform::Reversible53);
 
         assert_eq!(
             data,
