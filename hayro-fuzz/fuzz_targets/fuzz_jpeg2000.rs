2d316a3591b3b9579199f4915233814253b452c3
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2025-12-27 16:44:55 +0100

    hayro-fuzz: Update commands (#748)

--- hayro-fuzz/fuzz_targets/fuzz_jpeg2000.rs
+++ hayro-fuzz/fuzz_targets/fuzz_jpeg2000.rs
@@ -7,7 +7,10 @@ use libfuzzer_sys::fuzz_target;
 fuzz_target!(|data: &[u8]| {
     let settings = DecodeSettings::default();
     if let Ok(image) = Image::new(data, &settings) {
-        let mut buf = vec![0_u8; image.total_bytes() as usize];
-        let _ = image.read_image(&mut buf);
+        // Let's ignore larger images so we don't time out.
+        if image.width() <= 2500 && image.height() <= 2500 {
+            let mut buf = vec![0_u8; image.total_bytes() as usize];
+            let _ = image.read_image(&mut buf);
+        }
     }
 });
