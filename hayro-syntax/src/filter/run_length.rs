54b126098bcf25a8040de4133fa627c3ddeed4e3
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2026-02-01 11:23:33 +0100

    hayro-syntax: Improve code in run-length decoder (#918)

--- hayro-syntax/src/filter/run_length.rs
+++ hayro-syntax/src/filter/run_length.rs
@@ -1,32 +1,32 @@
 use crate::reader::Reader;
 use alloc::vec;
 use alloc::vec::Vec;
-use log::warn;
 
 pub(crate) fn decode(data: &[u8]) -> Option<Vec<u8>> {
     let mut reader = Reader::new(data);
     let mut decoded = vec![];
 
-    if decode_inner(&mut reader, &mut decoded).is_none() {
-        warn!("run-length decode stream ended prematurely");
-    }
-
-    Some(decoded)
-}
-
-fn decode_inner(reader: &mut Reader<'_>, decoded: &mut Vec<u8>) -> Option<()> {
     loop {
         let length = reader.read_byte()?;
 
         match length {
-            128 => return Some(()),
-            0..=127 => decoded.extend(reader.read_bytes(length as usize + 1)?),
+            128 => break,
+            0..=127 => {
+                // PDFBOX-3990, just abort early if stream is invalid.
+                let Some(bytes) = reader.read_bytes(length as usize + 1) else {
+                    break;
+                };
+
+                decoded.extend(bytes);
+            }
             _ => {
                 let length = 257 - length as usize;
                 decoded.extend([reader.read_byte()?].repeat(length));
             }
         }
     }
+
+    Some(decoded)
 }
 
 #[cfg(test)]
