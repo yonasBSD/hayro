97b2d939a6acc83c2adc96a8e8801f3d026136eb
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2026-04-06 19:53:11 +0200

    hayro-interpret: Only resolve f32 profiles lazily (#1170)

--- hayro-interpret/src/color.rs
+++ hayro-interpret/src/color.rs
@@ -879,8 +879,8 @@ struct ICCColorRepr {
     number_components: usize,
     is_srgb: bool,
     is_lab: bool,
-    transform_u8: OnceLock<Option<Arc<Transform8BitExecutor>>>,
-    transform_f32: OnceLock<Option<Arc<TransformF32Executor>>>,
+    transform_u8: Arc<Transform8BitExecutor>,
+    transform_f32: OnceLock<Arc<TransformF32Executor>>,
 }
 
 #[derive(Clone)]
@@ -924,13 +924,24 @@ impl ICCProfile {
             }
         };
 
+        let dest_profile = ColorProfile::new_srgb();
+        let transform_u8 = src_profile
+            .clone()
+            .create_transform_8bit(
+                src_layout,
+                &dest_profile,
+                Layout::Rgb,
+                TransformOptions::default(),
+            )
+            .ok()?;
+
         Some(Self(Arc::new(ICCColorRepr {
             src_profile,
             src_layout,
             number_components,
             is_srgb,
             is_lab,
-            transform_u8: OnceLock::new(),
+            transform_u8,
             transform_f32: OnceLock::new(),
         })))
     }
@@ -943,42 +954,28 @@ impl ICCProfile {
         self.0.is_lab
     }
 
-    fn transform_u8(&self) -> Option<&Arc<Transform8BitExecutor>> {
-        self.0
-            .transform_u8
-            .get_or_init(|| {
-                let dest_profile = ColorProfile::new_srgb();
-                self.0
-                    .src_profile
-                    .clone()
-                    .create_transform_8bit(
-                        self.0.src_layout,
-                        &dest_profile,
-                        Layout::Rgb,
-                        TransformOptions::default(),
-                    )
-                    .ok()
-            })
-            .as_ref()
+    fn transform_u8(&self) -> &Arc<Transform8BitExecutor> {
+        &self.0.transform_u8
     }
 
-    fn transform_f32(&self) -> Option<&Arc<TransformF32Executor>> {
-        self.0
-            .transform_f32
-            .get_or_init(|| {
-                let dest_profile = ColorProfile::new_srgb();
-                self.0
-                    .src_profile
-                    .clone()
-                    .create_transform_f32(
-                        self.0.src_layout,
-                        &dest_profile,
-                        Layout::Rgb,
-                        TransformOptions::default(),
-                    )
-                    .ok()
-            })
-            .as_ref()
+    fn transform_f32(&self) -> &Arc<TransformF32Executor> {
+        // From my benchmarking, creating the f32 transforms is usually much
+        // more expensive than u8. Therefore, we only create it lazily when
+        // really needed.
+        self.0.transform_f32.get_or_init(|| {
+            let dest_profile = ColorProfile::new_srgb();
+            self.0
+                .src_profile
+                .clone()
+                .create_transform_f32(
+                    self.0.src_layout,
+                    &dest_profile,
+                    Layout::Rgb,
+                    TransformOptions::default(),
+                )
+                // Since the u8 version was valid, hopefully this should never panic?
+                .unwrap()
+        })
     }
 }
 
@@ -998,9 +995,9 @@ impl ToRgb for ICCProfile {
                     ]
                 })
                 .collect::<Vec<_>>();
-            self.transform_f32()?.transform(&scaled, &mut temp).ok()?;
+            self.transform_f32().transform(&scaled, &mut temp).ok()?;
         } else {
-            self.transform_f32()?.transform(input, &mut temp).ok()?;
+            self.transform_f32().transform(input, &mut temp).ok()?;
         };
 
         for (input, output) in temp.iter().zip(output.iter_mut()) {
@@ -1018,7 +1015,7 @@ impl ToRgb for ICCProfile {
         if self.is_srgb() {
             output.copy_from_slice(input);
         } else {
-            self.transform_u8()?.transform(input, output).ok()?;
+            self.transform_u8().transform(input, output).ok()?;
         }
 
         Some(())
